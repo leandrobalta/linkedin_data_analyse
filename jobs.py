03a79dedfa10cee1adcf52253077c87c8467c23a
leandrobalta <leandrobraga.job@gmail.com>
2024-06-07 11:35:27 -0300

    fixed jobs.py

--- jobs.py
+++ jobs.py
@@ -21,23 +21,29 @@ def main():
         cursor = cnx.cursor()
 
         # Preparando a query de inserção
-        query = ("INSERT INTO jobs (job_id, company_name, title, description, location, company_id, med_salary, remote_allowed, work_type, application_url, expiry) "
+        query = ("INSERT INTO job (job_id, company_name, title, description, location, company_id, med_salary, remote_allowed, work_type, application_url, expiry) "
                      "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
       
         # Iterando sobre cada linha do DataFrame
-        for index, row in df_filtered.iterrows():
+        helper = 0
+        for index, row in df_final.iterrows():
+            for i in range(len(row)):
+                if pd.isna(row[i]):
+                    row[i] = None
+                
             data_to_insert = tuple(row)
             try:
                 cursor.execute(query, data_to_insert)
                 cnx.commit()
                 print(f"Linha {index + 1} inserida com sucesso.")
-                if index == 4000:
+                helper += 1
+                if helper == 4000:
                     break
             except mysql.connector.Error as err:
                 print(f"Erro ao inserir linha {index + 1}: {err}")
                 cnx.rollback()
 
-        cursor.execute("SELECT COUNT(*) FROM companies")
+        cursor.execute("SELECT COUNT(*) FROM job")
         result = cursor.fetchone()
         print(f"Total de registros na tabela: {result[0]}")
 
